a7ab04cc918908e5dc086be3e19245d0c24bd9b2
Sumit Kumar <tbeast935@gmail.com>
2026-02-24 09:17:04 +0530

    pract ques

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let a = 'sumit'
-console.log(a.charAt('i'));
+function abc(val){
+if(val.charCodeAt(0)>=65 && val.charCodeAt(0)<=90 ){
+    return 'Uppercase'
+}else if(val.charCodeAt(0)>=97 && val.charCodeAt(0)<=122){
+    return 'LowerCase'
+}else if(val.charCodeAt(0)>=48 && val.charCodeAt(0)<=57){
+    return 'Digit'
+}else{
+    return 'special char'
+}
+}
+console.log(abc("a"));
