2bbf0f3d253a2674d93fbe089b737ed23dfba05e
Sumit Kumar <tbeast935@gmail.com>
2026-02-26 23:04:47 +0530

    ppp

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function vowel(vow){
-    let output;
-    if (vow == "a"||vow == "e"||vow == "i"||vow == "o"||vow == "u"){
-        output="it is vowel"
-        
-    }else{
-        output="invalid"
-        
+function checkVowels(char = '0'){
+    if('aeiou'.includes(char.toLowerCase()) && char != ""){
+        return 'vowel'
+    } else if(/[a-z]/.test(char.toLowerCase())){
+        return 'Consonants'
     }
-    return output
+    return 'Not a Valid aplhabet'
 }
-console.log(vowel("c"));
