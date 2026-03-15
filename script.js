e2e7e8da9b9b7788110618e6de2319a865084658
Sumit Kumar <tbeast935@gmail.com>
2026-03-15 22:26:01 +0530

    prct

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const count = document.getElementById("count")
 count.addEventListener("input",function(e){
-    let inp = e.target.value;
+    let inp = e.target.value.length;
     // count.value = inp.length;
-    v
+    
 
    console.log(inp);
    
