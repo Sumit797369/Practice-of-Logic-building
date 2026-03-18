c4d56322161a6fab26c5ba69e06744a6f4dca1f5
Sumit Kumar <tbeast935@gmail.com>
2026-03-18 22:44:31 +0530

    prct

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let prm = new Promise((resolve, reject) => {
-  console.log("aa rha hai");
+// const input = document.getElementById('inp');
+
+// input.addEventListener("input",function(e){
+
+//   let value=e.target.value;
+//   let b=value.toUpperCase();
+//   console.log(b);
   
-setTimeout(()=>{
-  resolve("done")
-},200)
-});
-prm.then((res)=>{
-  console.log(res);
   
-})
+// })
+ 
+let a = "hello world";
+
+console.log(a.indexOf("world"));
